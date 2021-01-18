bdc70e35d08aafabb43d366f373f829ea77da086
April Howard <aaprilhoward515@gmail.com>
2021-01-17 20:26:44 -0500

    Cleaned up HiLo format. Added Thank you for playing comment

--- src/HiLo.java
+++ src/HiLo.java
@@ -3,25 +3,23 @@ import java.util.Scanner;
 public class HiLo {
 
 	public static void main(String[] args) {
-        String newLine = System.lineSeparator();
+		String newLine = System.lineSeparator();
 
-		
 		// get read for the player to use keyboard
-		
+
 		Scanner scanInput = new Scanner(System.in);
-		
+
 		// play again String variable
 		String playAgain = "";
 		// game loop
 		do {
-		
 			// create random number from 1 - 100
 			int theNumber = 1 + (int) (Math.random() * 100);
 
 			int usersGuess = 0;
 
 			while (usersGuess != theNumber) {
-				System.out.println("Guess a number between 1 and 100");
+				System.out.println("Guess a number between 1 and 100.");
 				usersGuess = scanInput.nextInt();
 				if (usersGuess < theNumber) {
 					System.out.println(newLine + "Oops. " + usersGuess + " too low.");
@@ -33,11 +31,10 @@ public class HiLo {
 
 			// ask player if play again
 			// ask for 'y' to play again
-
-			System.out.println(newLine + "Would you like to play again?");
-			System.out.println("Press Y for Yes or N for No");
+			System.out.println(newLine + "Would you like to play again (y/n)?");
 			playAgain = scanInput.next();
-		}
-		while(playAgain.equalsIgnoreCase("Y"));
-}
+		} while (playAgain.equalsIgnoreCase("Y"));
+	
+		System.out.println("Thank you for playing");
+	}
 }
